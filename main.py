31087421755c85f26175177907eb00f182c38416
Arhaan Bhiwandkar <arhaanbhiwandkar@gmail.com>
2023-12-21 09:14:41 +0530

    exception handeling + name check

--- main.py
+++ main.py
@@ -8,35 +8,55 @@ app.secret_key = "THALA"
 def home():
     numbers = ""
     final_val = None
+    thala = False
+    i = 0
     numbers = request.form.get("expression")
+    check = []
     if numbers:
+        numbers = numbers.strip()
+        ops = ["*","+","-","/","%"]
+        opsval = numbers.count(" ")
         i = 0
-        while True:
-            ans_val = ""
-            ops = ["*","+","-","/","%"]
-            opsval = numbers.count(" ")
+        while i!=(5**opsval):
             opslst = ""
+            ans_val = ""
             for x in range(opsval):
                 opslst += random.choice(ops)
             opslst += " "
-            numlist = list(map(int,numbers.split(" ")))
+            try:
+                numlist = list(map(int,numbers.split(" ")))
+            except ValueError:
+                if len(numbers) == 7:
+                    numlist = list(numbers)
+                    for x in range(len(numlist)):
+                        numlist[x]+="+"
+                    final_val = "".join(numlist)
+                    final_val = final_val[:-1]
+                    thala = True
+                    break
+                else:
+                    thala = False
+                    final_val = "Not Thala"
+                    break
             for x in range(len(numlist)):
                 ans_val += str(numlist[x])+opslst[x]
-            try:
-                if (eval(ans_val) == 7) or (eval(ans_val) == 7.0):
-                    print("Thala For a Reason")
-                    final_val = ans_val
-                    break
-            except ZeroDivisionError:
+            if opslst not in check:
+                check.append(opslst)
+                i+=1
+                try:
+                    if (eval(ans_val) == 7) or (eval(ans_val) == 7.0):
+                        print("Thala For a Reason")
+                        final_val = ans_val
+                        thala = True
+                except ZeroDivisionError:
+                    pass
+            else:
                 pass
-            i+=1
-            if (i == 100):
-                print("Break")
-                final_val == None
-                break
+        if thala == False and i == 5**opsval:
+            final_val = "Not Thala"
     else:
         numbers = ""
-    return render_template("index.html",final_val=final_val,numbers=numbers)
+    return render_template("index.html",final_val=final_val,numbers=numbers,thala=thala)
 
 if "__main__" == __name__:
     app.run(debug=True)
